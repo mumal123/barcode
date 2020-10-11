b6c601d3f1c3fdd1a56603ae4c2a6d63219d1a37
Mumal Swami <66372736+mumal123@users.noreply.github.com>
2020-10-11 14:54:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ScanScreen from "./screens/ScanScreen";
+import Useless from "./screens/Useless";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  ScanScreen: { screen: ScanScreen },
+ Useless: { screen: Useless }
+});
+
+const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
